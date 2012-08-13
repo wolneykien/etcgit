38379d7bd8c71be9cd9badf50d0e570126313e87
Paul Wolneykien <manowar@altlinux.ru>
2012-08-13 18:05:55 +0400

    (Re)-format the code

--- alterator-etcgit/ui/etcgit/log/ajax.scm
+++ alterator-etcgit/ui/etcgit/log/ajax.scm
@@ -12,55 +12,55 @@
 (define (format-row row proc)
   (if (plist? row)
     (let* ((ns-vs (plist-fold
-	            (lambda (n v ns-vs)
-	              (cons
-	                (append (car ns-vs) (list n))
-	                (append (cdr ns-vs) (list v))))
-		    (cons '() '())
-		    row))
+                    (lambda (n v ns-vs)
+                      (cons
+                        (append (car ns-vs) (list n))
+                        (append (cdr ns-vs) (list v))))
+                    (cons '() '())
+                    row))
            (res (apply proc (cdr ns-vs)))
-	   (ns (car ns-vs))
-	   (vs (list-head res (length ns)))
-	   (aux (list-tail res (length vs))))
+           (ns (car ns-vs))
+           (vs (list-head res (length ns)))
+           (aux (list-tail res (length vs))))
       (append
         (fold (lambda (n v rlist)
-	        (append rlist (list n v)))
-	      '()
-	      ns
-	      vs)
-	aux))
+                (append rlist (list n v)))
+              '()
+              ns
+              vs)
+        aux))
     (cons (car row) (format-row (cdr row) proc))))
 
 (define (format-commit head timestamp msg)
   (list head
-	(date->string
-	  (time-utc->date
-	    (date->time-utc
-	      (string->date timestamp "~Y-~m-~d ~H:~M:~S ~z")))
-	  "~d.~m.~Y ~H:~M:~S")
-	msg))
+        (date->string
+          (time-utc->date
+            (date->time-utc
+              (string->date timestamp "~Y-~m-~d ~H:~M:~S ~z")))
+          "~d.~m.~Y ~H:~M:~S")
+        msg))
 
 (define (read-log)
   (catch/message
     (lambda ()
       (let* ((branch (form-value "branch"))
-	     (log (woo-list "/etcgit/commits" 'head branch
-			                      'limit (+ *limit* 1)
-					      'skip *skip*
-					      'branch branch)))
-	(if (> (length log) *limit*)
-	    (begin
-	      (form-update-enum "log"
-				(map (lambda (row)
-				       (format-row row format-commit))
-				     (list-head log *limit*)))
-	      #t)
-	    (begin
-	      (form-update-enum "log"
-				(map (lambda (row)
-				       (format-row row format-commit))
-				     log))
-	      #f))))))
+             (log (woo-list "/etcgit/commits" 'head branch
+                            'limit (+ *limit* 1)
+                            'skip *skip*
+                            'branch branch)))
+        (if (> (length log) *limit*)
+          (begin
+            (form-update-enum "log"
+                              (map (lambda (row)
+                                     (format-row row format-commit))
+                                   (list-head log *limit*)))
+            #t)
+          (begin
+            (form-update-enum "log"
+                              (map (lambda (row)
+                                     (format-row row format-commit))
+                                   log))
+            #f))))))
 
 (define (read-repo)
   (catch/message
@@ -84,10 +84,10 @@
   (form-bind "prev" "click"
     (lambda ()
       (if (> *skip* 0)
-	  (set! *skip*
-		(if (> *skip* *limit*)
-		    (- *skip* *limit*)
-		    0)))
+        (set! *skip*
+          (if (> *skip* *limit*)
+            (- *skip* *limit*)
+            0)))
       (form-update-visibility "prev" (> *skip* 0))
       (form-update-visibility "next" (read-log))))
   (form-bind "next" "click"
